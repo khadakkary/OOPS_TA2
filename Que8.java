78a0cc5bd5ad4b609ec60b5efe729a5958283f31
khadakkary <24khadakkary@rbunagpur.in>
2025-04-18 23:27:03 +0530

    Create Que8.java

---
+++ Que8.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main8 {
+    public static void main(String[] args) {
+        try {
+            // Writing to a file
+            FileWriter writer = new FileWriter("myfile.txt");
+            writer.write("This is Java File Handling.");
+            writer.close();
+            System.out.println("File successfully written.");
+
+            // Reading from the file
+            FileReader reader = new FileReader("myfile.txt");
+            int ch;
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("An error has occurred: " + e.getMessage());
+        }
+    }
+}
